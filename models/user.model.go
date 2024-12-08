48785240fd46f7d6b31448cdbe956b3a41e688e0
Hajid Raihan <hajidraihan@gmail.com>
2024-12-08 17:37:07 +0800

    create: getUserById endpoint

---
+++ models/user.model.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	// BornDate time.Time `json:"born_date"`
+}
--- routes/index.route.go
+++ routes/index.route.go
@@ -9,5 +9,11 @@ import (
 func InitRoutes(app *gin.Engine) {
 
 	route := app
+
 	route.GET("/", user_controller.GetAllUser)
+	route.GET("/:id", user_controller.GetUserById)
+	route.POST("/add", user_controller.AddUser)
+	route.PUT("/update/:id", user_controller.UpdateUser)
+	route.DELETE("/delete/:id", user_controller.DeleteUser)
+	route.GET("/hello", user_controller.Hello)
 }
